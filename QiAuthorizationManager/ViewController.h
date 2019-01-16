dcc5d96141af9dcfe3fa752cbbe2d2c746a4f15d
QiShare <1928386499@qq.com>
2019-01-16 16:06:46 +0800

    add project

---
+++ QiAuthorizationManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiAuthorizationManager
+//
+//  Created by huangxianshuai on 2019/1/16.
+//  Copyright © 2019年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
